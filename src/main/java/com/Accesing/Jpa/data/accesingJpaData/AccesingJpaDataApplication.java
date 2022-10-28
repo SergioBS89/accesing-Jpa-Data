7adae72db410231b5fc07cd15850a93b8cca79f4
sergio berdiell sanchez <sergiovencillon10@gmail.com>
2022-10-28 11:10:10 +0000

    AccesingJpa

---
+++ src/main/java/com/Accesing/Jpa/data/accesingJpaData/AccesingJpaDataApplication.java
@@ -0,0 +1,13 @@
+package com.Accesing.Jpa.data.accesingJpaData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesingJpaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesingJpaDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Accesing/Jpa/data/accesingJpaData/AccesingJpaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Accesing.Jpa.data.accesingJpaData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesingJpaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
